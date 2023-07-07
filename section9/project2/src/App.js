0d5a5b544f5265c923979202b903946a3d2f4125
daewoong <dez20.kim@samsung.com>
2023-07-07 19:14:52 +0900

    WIP: project2

---
+++ section9/project2/src/App.js
@@ -0,0 +1,12 @@
+import AddUser from "./components/AddUser";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <Card>
+      <AddUser />
+    </Card>
+  );
+}
+
+export default App;
