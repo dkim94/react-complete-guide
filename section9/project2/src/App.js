74d25c678c57edfe7f65cd7ead4d3188f9e1557f
daewoong <dez20.kim@samsung.com>
2023-07-07 20:02:51 +0900

    Completed project2

--- section9/project2/src/App.js
+++ section9/project2/src/App.js
@@ -1,11 +1,24 @@
-import AddUser from "./components/AddUser";
-import Card from "./components/Card";
+import { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const submitHandler = (userName, age) => {
+    console.log("Called App.submitHandler");
+    setUsersList((prevList) => {
+      return [...prevList, { name: userName, age: age, id: Math.random() }];
+    });
+    console.log(usersList);
+  };
+
   return (
-    <Card>
-      <AddUser />
-    </Card>
+    <div>
+      <AddUser onSubmit={submitHandler} />
+      {usersList.length > 0 && <UsersList list={usersList} />}
+    </div>
   );
 }
 
