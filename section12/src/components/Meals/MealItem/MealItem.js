64249c0e7c172e48853bcd90180f6aaf2bbfa49d
daewoong <dez20.kim@samsung.com>
2023-07-24 22:02:24 +0900

    Section 12 complete

--- section12/src/components/Meals/MealItem/MealItem.js
+++ section12/src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,16 @@
+import { useContext } from "react";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
 import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext)
   const price = `$${props.price.toFixed(2)}`;
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({id: props.id, name: props.name, amount: amount, price: props.price});
+  };
+
   return (
     <>
       <li className={classes.meal}>
@@ -12,7 +19,9 @@ const MealItem = (props) => {
           <div className={classes.description}>{props.description}</div>
           <div className={classes.price}>{price}</div>
         </div>
-        <div><MealItemForm id={props.id}/></div>
+        <div>
+          <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
+        </div>
       </li>
     </>
   );
